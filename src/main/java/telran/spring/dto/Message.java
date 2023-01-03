1a78d3eb50d53d08e12b1fc9164e8bc683c369c1
StasSaltykov <stasyanych92@gmail.com>
2023-01-03 21:18:41 +0200

    CW #54

---
+++ src/main/java/telran/spring/dto/Message.java
@@ -0,0 +1,14 @@
+package telran.spring.dto;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION)
+@JsonSubTypes({@Type(EmailMessage.class), @Type(SmsMessage.class), @Type(TcpMessage.class)})
+public class Message {
+	
+	public String type;
+	public String text;
+
+}
